0cc0400a3e9d45a4585ea8403d1e10754ae7db60
woookk <woook27@naver.com>
2023-07-26 16:30:43 +0900

    Feat: 초기 프로젝트 설정

---
+++ src/main/java/org/swmaestro/mohaeng/MoHaengApplication.java
@@ -0,0 +1,13 @@
+package org.swmaestro.mohaeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoHaengApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoHaengApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/swmaestro/mohaeng/MoHaengApplicationTests.java
@@ -0,0 +1,13 @@
+package org.swmaestro.mohaeng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoHaengApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
