93c8b85e5c2d935a88af35ae03490d58718cb8b1
woookk <woook27@naver.com>
2023-08-10 15:27:57 +0900

    Test: BaseTimeEntity, User Entity 테스트 추가

---
+++ src/test/java/org/swmaestro/mohaeng/domain/user/UserTest.java
@@ -0,0 +1,36 @@
+package org.swmaestro.mohaeng.domain.user;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    @Test
+    @DisplayName("모든 정보를 올바른 형식으로 입력하면 회원이 생성된다")
+    void createUser() {
+        assertDoesNotThrow(
+                () -> User.createUser("test@mohaeng.org", "test", "test", "test", "test")
+        );
+    }
+
+    @Test
+    @DisplayName("회원의 프로필 이미지가 존재하면 해당 이미지 url을 올바르게 반환한다")
+    void getImgUrlWhenHasImage() {
+        String expected = "testImage.jpg";
+        User user = User.createUser("test@mohaeng.org", "test", "test", "test", expected);
+
+        String actual = user.getImageUrl();
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("회원 프로필 이미지를 변경하면 변경 전 이미지는 사용여부를 false로 변경하고 프로필 이미지는 올바르게 변경된다.")
+    void updateProfileImage() {
+        String expected = "testImage.jpg";
+        User user = User.createUser("test@mohaeng.org", "test", "test", "test", "beforeImage.jpg");
+    }
+}
