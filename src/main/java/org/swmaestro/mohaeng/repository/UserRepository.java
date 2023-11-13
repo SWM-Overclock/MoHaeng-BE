63ff82a5605302c6880335104077f61836e765ba
woookk <woook27@naver.com>
2023-11-14 03:08:57 +0900

    Refactor: 필요없는 import 제거

--- src/main/java/org/swmaestro/mohaeng/repository/UserRepository.java
+++ src/main/java/org/swmaestro/mohaeng/repository/UserRepository.java
@@ -1,7 +1,6 @@
 package org.swmaestro.mohaeng.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import org.swmaestro.mohaeng.domain.user.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
