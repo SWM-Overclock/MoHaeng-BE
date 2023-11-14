672c9371efceb15970f00e74f77e1a8342f0dac7
woookk <woook27@naver.com>
2023-11-14 16:25:04 +0900

    Feat: AuthController의 Access Token 재발급 엔드포인트 추가

---
+++ src/main/java/org/swmaestro/mohaeng/controller/AuthController.java
@@ -0,0 +1,25 @@
+package org.swmaestro.mohaeng.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.swmaestro.mohaeng.component.jwt.JwtTokenProvider;
+import org.swmaestro.mohaeng.dto.TokenRequest;
+import org.swmaestro.mohaeng.dto.TokenResponse;
+
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/auth")
+@RestController
+public class AuthController {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @PostMapping("/reissue")
+    public ResponseEntity<?> reissueToken(@RequestBody TokenRequest tokenRequest) {
+        log.info("refreshToken: {}", tokenRequest.getRefreshToken());
+        String newAccessToken = jwtTokenProvider.reissueAccessToken(tokenRequest.getRefreshToken());
+        return ResponseEntity.ok(new TokenResponse(newAccessToken));
+    }
+}
