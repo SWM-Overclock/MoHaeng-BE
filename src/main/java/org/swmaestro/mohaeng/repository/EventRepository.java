9e56d8a43bd33b5e7b2e43c9cdbf4e40c3244db8
woookk <woook27@naver.com>
2023-11-23 03:28:37 +0900

    Refactor: @Query import문 수정

--- src/main/java/org/swmaestro/mohaeng/repository/EventRepository.java
+++ src/main/java/org/swmaestro/mohaeng/repository/EventRepository.java
@@ -1,8 +1,8 @@
 package org.swmaestro.mohaeng.repository;
 
-import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.swmaestro.mohaeng.domain.event.Event;
 import org.swmaestro.mohaeng.domain.event.EventType;
 
