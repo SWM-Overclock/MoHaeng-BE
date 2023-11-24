be272ff0afb5f28691696f2c297630d0c9ba891b
woookk <woook27@naver.com>
2023-11-24 22:57:43 +0900

    Refactor: ShopDetailRepository에서 ShopRepository로 위치 기반 Shop 조회 쿼리 이동

--- src/main/java/org/swmaestro/mohaeng/repository/ShopRepository.java
+++ src/main/java/org/swmaestro/mohaeng/repository/ShopRepository.java
@@ -1,7 +1,19 @@
 package org.swmaestro.mohaeng.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.swmaestro.mohaeng.domain.shop.Shop;
 
+import java.util.List;
+
 public interface ShopRepository extends JpaRepository<Shop, Long> {
+
+    @Query(value = "SELECT DISTINCT sd.shop FROM shop_detail sd " +
+            "WHERE ST_DISTANCE_SPHERE(" +
+            "point(sd.shop_longitude, sd.shop_latitude), " +
+            "point(:longitude, :latitude)) <= :radius " +
+            "AND sd.status = 'ACTIVE'",
+            nativeQuery = true)
+    List<Shop> findDistinctShopsWithinRadius(@Param("longitude") double longitude, @Param("latitude") double latitude, @Param("radius") double radius);
 }
